f61fd7484e64fdd797618be88b039c0a6caf5b0e
Vladyslav Kucherenko <vlados.soc@gmail.com>
2025-01-13 17:56:26 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, astuple, fields
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup, Tag
+import requests
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -8,8 +15,48 @@ class Quote:
     tags: list[str]
 
 
+FIELDS_LIST = [field.name for field in fields(Quote)]
+
+
+def parse_single_quote(quote: Tag) -> Quote:
+    text = quote.select_one(".text").text
+    author = quote.select_one(".author").text
+    tags_html = quote.select(".tag")
+    return Quote(
+        text=text,
+        author=author,
+        tags=[tag.text for tag in tags_html]
+    )
+
+
+def get_quotes_from_page(base_url: str) -> list[Quote]:
+    text = requests.get(base_url).text
+    soup = BeautifulSoup(text, "html.parser")
+    quotes = soup.select(".quote")
+    next_page = soup.select_one(".next a")
+    while next_page:
+
+        url_to_parse = urljoin(BASE_URL, next_page.get("href"))
+
+        text = requests.get(url_to_parse).text
+        soup = BeautifulSoup(text, "html.parser")
+        new_quotes = soup.select(".quote")
+        quotes.extend(new_quotes)
+        next_page = soup.select_one(".next a")
+
+    return [parse_single_quote(quote) for quote in quotes]
+
+
+def write_quotes_to_csv(path: str, quotes: list[Quote]) -> None:
+    with open(path, "w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(FIELDS_LIST)
+        writer.writerows(astuple(quote) for quote in quotes)
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_quotes_from_page(BASE_URL)
+    write_quotes_to_csv(output_csv_path, quotes)
 
 
 if __name__ == "__main__":
